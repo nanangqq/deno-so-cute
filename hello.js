afc41e99a2c6e746a930b1e92abf51d62a0f088f
nanangqq <gingggg@gmail.com>
2021-09-11 15:35:03 +0900

    event.isTrusted

--- hello.js
+++ hello.js
@@ -1,5 +1,6 @@
 addEventListener('fetch', event => {
-  const msg = `${Object.keys(event).join(',')}`
+  // const msg = `${Object.keys(event).join(',')}` // isTrusted 하나 나옴,,
+  const msg = `${event.isTrusted}`
   const response = new Response(msg, {
     headers: { 'content-type': 'text/plain' },
   })
