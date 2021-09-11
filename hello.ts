12ca75cbb406967059823e641f73d88b3df1f2de
nanangqq <gingggg@gmail.com>
2021-09-11 16:29:16 +0900

    env test

--- hello.ts
+++ hello.ts
@@ -6,7 +6,7 @@ addEventListener('fetch', event => {
   const name: name = { name: 'name' }
   // const msg = `${Object.keys(event).join(',')}` // isTrusted 하나 나옴,,
   // const msg = `${event.isTrusted}` // false
-  const msg = `hello ${name.name}` // false
+  const msg = `hello ${name.name}, ${Deno.env.get('haha')}`
   const response = new Response(msg, {
     headers: { 'content-type': 'text/plain' },
   })
