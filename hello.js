6def43153ee79c22ed0b212cedb23a3d1eec996d
nanangqq <gingggg@gmail.com>
2021-09-11 15:25:41 +0900

    console 찍히나?

--- hello.js
+++ hello.js
@@ -1,6 +1,8 @@
+console.log('hi')
 addEventListener('fetch', event => {
   const response = new Response('Hello Deno!', {
     headers: { 'content-type': 'text/plain' },
   })
+  console.log(event)
   event.respondWith(response)
 })
