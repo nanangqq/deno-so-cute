996849c077eacc8c0acf5a9c1987f3256d0e9beb
nanangqq <gingggg@gmail.com>
2021-09-11 16:53:32 +0900

    ip주소 가져오기 헤더 확인

--- ip.js
+++ ip.js
@@ -1,7 +1,9 @@
 addEventListener('fetch', event => {
   // Get the value of the header named `x-forwarded-for`.
   // The value can either be a IPv4 or IPv6 address.
-  const ip = event.request.headers.get('x-forwarded-for')
+  // const ip = event.request.headers.get('x-forwarded-for')
+  const ip = event.request.headers.get('host')
+  // 로컬에서 deployctl 로 실행시켜보면, ip주소 안나오고 null 로 나옴. 로컬로 요청 보낼때는 헤더에 `x-forwarded-for`값이 안들어가나봄. 'host'값은 제대로 찍힘
   const response = new Response(`Your IP address is <b>${ip}</b>`, {
     headers: { 'content-type': 'text/html' },
   })
