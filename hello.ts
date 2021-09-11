dc7aa7fb7bff97521d6ca7da87cc9692ee560e78
nanangqq <gingggg@gmail.com>
2021-09-11 15:43:30 +0900

    ts 되긴 되는 듯?

--- hello.ts
+++ hello.ts
@@ -1,6 +1,12 @@
+type name = {
+  name: string
+}
+
 addEventListener('fetch', event => {
+  const name: name = { name: 'name' }
   // const msg = `${Object.keys(event).join(',')}` // isTrusted 하나 나옴,,
-  const msg = `${event.isTrusted}` // false
+  // const msg = `${event.isTrusted}` // false
+  const msg = `hello ${name.name}` // false
   const response = new Response(msg, {
     headers: { 'content-type': 'text/plain' },
   })
